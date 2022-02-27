345f20bcc94cfeb7d0cdf768f8148c97d899905a
MrRahmat <vovacet12@mail.ru>
2022-02-28 01:27:48 +0300

    Start working with the header

---
+++ components/Center.jsx
@@ -0,0 +1,23 @@
+import { ChevronDownIcon } from "@heroicons/react/outline";
+import { useSession } from "next-auth/react";
+
+function Center() {
+    const { data: session } = useSession();
+  return (
+    <div className="flex-grow text-white">
+        <header className="absolute top-5 right-8">
+            <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2">
+                <img src={session?.user.image} className='w-10 h-10 rounded-full' alt={session?.user.name}/>
+                <h2>{session?.user.name}</h2>
+                <ChevronDownIcon className="h-5 w-5"/>
+            </div>
+        </header>
+
+        <section className={`flex items-end space-x-7 bg-gradient-to-b to-black from-red-500 h-80 text-white p-8`}>
+            <h1>Play the song!</h1>
+        </section>
+    </div>
+  )
+}
+
+export default Center
