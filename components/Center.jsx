bb1c45d997a98e397db0352c70ccb4a7dad47aa0
MrRahmat <vovacet12@mail.ru>
2022-03-01 19:45:00 +0300

    Added lodash

--- components/Center.jsx
+++ components/Center.jsx
@@ -1,8 +1,27 @@
+import { useState, useEffect } from "react";
 import { ChevronDownIcon } from "@heroicons/react/outline";
 import { useSession } from "next-auth/react";
+import { shuffle } from 'lodash';
+
+const colors = [
+    'from-indigo-500',
+    'from-pink-500',
+    'from-yellow-500',
+    'from-blue-500',
+    'from-red-500',
+    'from-purple-500',
+    'from-green-500'
+];
+
 
 function Center() {
     const { data: session } = useSession();
+    const [color, setColor] = useState(null);
+
+    useEffect(() => {
+      setColor(shuffle(colors).pop());
+    }, [])
+    
   return (
     <div className="flex-grow text-white">
         <header className="absolute top-5 right-8">
@@ -13,7 +32,7 @@ function Center() {
             </div>
         </header>
 
-        <section className={`flex items-end space-x-7 bg-gradient-to-b to-black from-red-500 h-80 text-white p-8`}>
+        <section className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white p-8`}>
             <h1>Play the song!</h1>
         </section>
     </div>
